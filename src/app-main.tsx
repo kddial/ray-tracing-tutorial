b2368149f2766036618e9cab7159be59b8ac242c
Kevin Dial <kevin.dial.phone@gmail.com>
2020-12-05 20:14:11 -0500

    add canvas

--- src/app-main.tsx
+++ src/app-main.tsx
@@ -1,3 +1,27 @@
+import { useEffect, useRef } from 'react';
+
 export default function AppMain() {
-  return <div>kevin</div>;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    // Configuration
+    const WIDTH = 128;
+    const HEIGHT = 128;
+
+    if (canvasRef.current != null) {
+      // Set up the canvas with a 2D rendering context
+      const canvas = canvasRef.current || {};
+      canvas.width = WIDTH;
+      canvas.height = HEIGHT;
+
+      const context = canvas.getContext('2d');
+      if (context) context.imageSmoothingEnabled = false;
+    }
+  }, []);
+
+  return (
+    <div className="canvas-container">
+      <canvas id="canvas" ref={canvasRef}></canvas>
+    </div>
+  );
 }
