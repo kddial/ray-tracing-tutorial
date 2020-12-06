d753cd390e4108673bef963d306a874e34264c9b
Kevin Dial <kevin.dial.phone@gmail.com>
2020-12-06 00:22:36 -0500

    add sky color

--- src/canvas-main.tsx
+++ src/canvas-main.tsx
@@ -1,17 +1,43 @@
-import Color from './color';
 import { WIDTH, HEIGHT } from './app-main';
+import Vector from './vector';
+import Ray from './ray';
 
 export function canvasMain(context: CanvasRenderingContext2D) {
-  for (let j = 0; j < HEIGHT; j++) {
-    console.log('Scanlines remaining', HEIGHT - j);
+  // image
+  const imageWidth = WIDTH;
+  const imageHeight = HEIGHT;
 
+  // camera
+  const viewportHeight = 2;
+  const viewportWidth = 2;
+  const focalLength = 1;
+
+  const origin = new Vector(0, 0, 0);
+  const horizontal = new Vector(viewportWidth, 0, 0);
+  const vertical = new Vector(0, viewportHeight, 0);
+
+  const lowerLeftCorner = origin
+    .subtract(horizontal.divide(2))
+    .subtract(vertical.divide(2))
+    .subtract(new Vector(0, 0, focalLength));
+
+  for (let j = 0; j < HEIGHT; j++) {
+    // html canvas (0,0) is in the top left corner.
+    // but we want the (0,0) to be the bottom left corner, so we flip j value for plotting
+    const jFlipped = HEIGHT - j;
+    console.log('Scanlines remaining', jFlipped);
     for (let i = 0; i < WIDTH; i++) {
-      const temp_color = new Color(i / (WIDTH - 1), j / (HEIGHT - 1), 0.25);
-      temp_color.draw(context, i, j);
+      const u = i / (imageWidth - 1);
+      const v = j / (imageHeight - 1);
+      const ray = new Ray(
+        origin,
+        lowerLeftCorner
+          .add(horizontal.multiply(u))
+          .add(vertical.multiply(v))
+          .subtract(origin),
+      );
+      ray.color().draw(context, i, jFlipped);
     }
   }
   console.log('Done.');
-
-  const color1 = new Color(1, 1, 1);
-  color1.draw(context, 100, 100);
 }
--- src/ray.tsx
+++ src/ray.tsx
@@ -1,6 +1,7 @@
 import Vector from './vector';
+import Color from './color';
 
-class Ray {
+export default class Ray {
   origin: Vector;
   direction: Vector;
 
@@ -11,5 +12,13 @@ class Ray {
 
   at(t: number) {
     return this.direction.multiply(t).add(this.origin);
+  }
+
+  color(): Color {
+    const unit_direction = this.direction.unit();
+    const t = 0.5 * (unit_direction.y + 1);
+    const color1 = new Color(1, 1, 1).multiply(1 - t);
+    const color2 = new Color(0.5, 0.7, 1.0).multiply(t);
+    return color1.add(color2);
   }
 }
