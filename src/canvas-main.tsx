0546bfcad67c18bd1c37a6efaa96ed72806d9247
Kevin Dial <kevin.dial.phone@gmail.com>
2020-12-06 22:48:50 -0500

    add time to render

--- src/canvas-main.tsx
+++ src/canvas-main.tsx
@@ -21,11 +21,11 @@ export function canvasMain(context: CanvasRenderingContext2D) {
     .subtract(vertical.divide(2))
     .subtract(new Vector(0, 0, focalLength));
 
+  const startTime = Date.now();
   for (let j = 0; j < HEIGHT; j++) {
     // html canvas (0,0) is in the top left corner.
     // but we want the (0,0) to be the bottom left corner, so we flip j value for plotting
     const jFlipped = HEIGHT - j;
-    console.log('Scanlines remaining', jFlipped);
     for (let i = 0; i < WIDTH; i++) {
       const u = i / (imageWidth - 1);
       const v = j / (imageHeight - 1);
@@ -39,5 +39,7 @@ export function canvasMain(context: CanvasRenderingContext2D) {
       ray.color().draw(context, i, jFlipped);
     }
   }
-  console.log('Done.');
+  const endTime = Date.now();
+  console.log(`Done in ${endTime - startTime} ms.`);
+  console.log(`Should aim for 16ms.`);
 }
